ecac1c9d945c46c358ef42091fb9e44f2fb1d1c5
VinayakS2005 <149326931+VinayakS2005@users.noreply.github.com>
2025-04-22 19:25:01 +0530

    Update scan.py

--- core/scan.py
+++ core/scan.py
@@ -2,36 +2,43 @@ def scan(requests, head, direction="right", disk_size=200):
     total_seek_time = 0
     sequence = []
     requests_sorted = sorted(requests)
+
     if direction == "right":
-        for i in range(head, disk_size):
-            if i in requests_sorted:
-                sequence.append(i)
-                total_seek_time += abs(head - i)
-                head = i
-                requests_sorted.remove(i)
-        sequence.append(disk_size - 1)
-        total_seek_time += abs(head - (disk_size - 1))
-        head = disk_size - 1
-        for i in range(disk_size - 1, -1, -1):
-            if i in requests_sorted:
-                sequence.append(i)
-                total_seek_time += abs(head - i)
-                head = i
-                requests_sorted.remove(i)
+        right = [r for r in requests_sorted if r >= head]
+        left = [r for r in requests_sorted if r < head]
+
+        for r in right:
+            total_seek_time += abs(head - r)
+            sequence.append(r)
+            head = r
+
+        if head != disk_size - 1:
+            total_seek_time += abs(head - (disk_size - 1))
+            sequence.append(disk_size - 1)
+            head = disk_size - 1
+
+        for r in reversed(left):
+            total_seek_time += abs(head - r)
+            sequence.append(r)
+            head = r
+
     else:
-        for i in range(head, -1, -1):
-            if i in requests_sorted:
-                sequence.append(i)
-                total_seek_time += abs(head - i)
-                head = i
-                requests_sorted.remove(i)
-        sequence.append(0)
-        total_seek_time += abs(head - 0)
-        head = 0
-        for i in range(0, disk_size):
-            if i in requests_sorted:
-                sequence.append(i)
-                total_seek_time += abs(head - i)
-                head = i
-                requests_sorted.remove(i)
+        left = [r for r in requests_sorted if r <= head]
+        right = [r for r in requests_sorted if r > head]
+
+        for r in reversed(left):
+            total_seek_time += abs(head - r)
+            sequence.append(r)
+            head = r
+
+        if head != 0:
+            total_seek_time += abs(head - 0)
+            sequence.append(0)
+            head = 0
+
+        for r in right:
+            total_seek_time += abs(head - r)
+            sequence.append(r)
+            head = r
+
     return sequence, total_seek_time
